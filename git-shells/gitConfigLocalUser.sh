498d253d5c3cf4922568bd8cef6dacfb47212d1e
bonkeenu <bonkeenu.tech@gmail.com>
2018-04-08 22:40:08 +0900

    change config string

--- git-shells/gitConfigLocalUser.sh
+++ git-shells/gitConfigLocalUser.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 . config
 
-echo "ローカル登録するUsername は ${username}です."
-echo "ローカル登録するメールアドレスは${mail} です."
+echo "ローカル登録するUsername は"${username}"です."
+echo "ローカル登録するメールアドレスは"${mail}"です."
 echo "configの設定をしますか？ [Y/n]"
 read ANSWER
 
