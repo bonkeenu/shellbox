baf9671c9a8969bf960207ba4575f6b29b18272a
bonkeenu <17017449+bonkeenu@users.noreply.github.com>
2018-04-11 11:09:42 +0900

    changed styles and added path modules

--- git-shells/gitConfigLocalUser.sh
+++ git-shells/gitConfigLocalUser.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
-. config
 
-echo "ローカル登録するUsername は"${username}"です."
-echo "ローカル登録するメールアドレスは"${mail}"です."
-echo "configの設定をしますか？ [Y/n]"
+# Getting config path and Reading config file.
+path=$(cd $(dirname $0) && pwd)
+. "$path/config"
+
+# Check your infomations.
+echo "ローカル登録するUsername は $username です."
+echo "ローカル登録するメールアドレスは $mail です."
+
+echo ".git/config の設定をしますか？ [Y/n]"
 read ANSWER
 
 case $ANSWER in
     "Y" | "y" | "yes" | "Yes" | "YES" )
-git config --local user.name ${username} 
-git config --local user.email ${mail} ;;
-    * ) echo "設定をキャンセルしました。";;
+git config --local user.name $username
+git config --local user.email $mail ;;
+    * ) printf "\e[33;41;1m設定をキャンセルしました. \e[m";;
 esac
 
